64566fd0b5c24e1bf15b0c59f129fae356e9b1c2
ESCID94 <kike15j@gmail.com>
2019-05-14 12:01:55 +0200

    update getters and setters

--- PhoneBook_Grupo3/src/main/java/com/lucatic/grupo3/model/Persona.java
+++ PhoneBook_Grupo3/src/main/java/com/lucatic/grupo3/model/Persona.java
@@ -8,7 +8,6 @@ import javax.persistence.Id;
 import lombok.Data;
 
 @Entity
-@Data
 public class Persona {
 
 	@Id
@@ -40,6 +39,60 @@ public class Persona {
 		this.apellido2 = apellido2;
 		this.dni = dni;
 		this.fechanacimiento = fechanacimiento;
+	}
+
+	public long getIdpersona() {
+		return idpersona;
+	}
+
+	public void setIdpersona(long idpersona) {
+		this.idpersona = idpersona;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido1() {
+		return apellido1;
+	}
+
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	public String getApellido2() {
+		return apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public Date getFechanacimiento() {
+		return fechanacimiento;
+	}
+
+	public void setFechanacimiento(Date fechanacimiento) {
+		this.fechanacimiento = fechanacimiento;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [idpersona=" + idpersona + ", nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2="
+				+ apellido2 + ", dni=" + dni + ", fechanacimiento=" + fechanacimiento + "]";
 	}
 
 
