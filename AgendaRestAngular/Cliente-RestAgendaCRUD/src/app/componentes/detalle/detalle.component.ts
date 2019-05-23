e5a2c81cca4dfe86c5fbca57c1854da82dabde5f
NaderZaiter <zaayter99@gmail.com>
2019-05-23 14:39:40 +0200

    personDetails

--- AgendaRestAngular/Cliente-RestAgendaCRUD/src/app/componentes/detalle/detalle.component.ts
+++ AgendaRestAngular/Cliente-RestAgendaCRUD/src/app/componentes/detalle/detalle.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/modelo/persona/persona';
+import { PersonaServiciosService } from 'src/app/servicios/persona-servicios.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   styleUrls: ['./detalle.component.css']
 })
+
+
 export class DetalleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private personaServicio:PersonaServiciosService) { }
 
   ngOnInit() {
+    
   }
+
+  detallesPersona(id:number): void {
+    this.personaServicio.detailPersona;
+  };
 
 }
